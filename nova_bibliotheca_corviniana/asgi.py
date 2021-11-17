d9e6ceedcc0415541c2200aeb14ce414a70f8b88
brian kasten <panphobicdream@gmail.com>
2021-11-17 17:49:23 -0500

    initial setup

---
+++ nova_bibliotheca_corviniana/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nova_bibliotheca_corviniana project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nova_bibliotheca_corviniana.settings')
+
+application = get_asgi_application()
